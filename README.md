d6e253be0d75af8974a98039ce8042fce4d8dbc0
daniel anggrianto <d.anggrianto@gmail.com>
2019-05-30 11:34:19 -0400

    packaging

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # export_yaml
 
+`export_yaml` is a python scripts to read yaml file and convert it to bash file
+to be sourced later
+
+## Installation
+
+`pip install export-yaml`
+
+## How to use
+
 ```
 export YAML_FILE=path_to_yaml
-source $(export.py)
+source $(export_yaml)
 ```
+
+## YAML File configuration
+```
+---
+env:
+  HELLO: "world"
+```
+Note: `export_yaml` is going only to read everything under `env`
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="export-yaml",
+    name="export_yaml",
     version="0.0.1",
     author="Example Author",
     author_email="d.anggrianto@gmail.com",
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/danggrianto/export_yaml/",
     packages=setuptools.find_packages(),
+    scripts=['export_yaml/export_yaml'],
+    install_requires=['pyyaml'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
